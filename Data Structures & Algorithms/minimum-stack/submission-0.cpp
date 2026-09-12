4b78fcac290a808c9fd52c7a3c9ca783c66818f1
Darshan <darshanm0309@gmail.com>
2026-09-12 10:47:13 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.cpp
@@ -0,0 +1,34 @@
+class MinStack {
+public:
+    vector<int> st;
+
+    MinStack() {
+
+    }
+    
+    void push(int val) {
+        st.push_back(val);
+
+    }
+    
+    void pop() {
+        int n = st.size()-1;
+        st.erase(st.begin()+n);
+    }
+    
+    int top() {
+        int n = st.size()-1;
+        int top = st[n];
+        return top;
+    }
+    
+    int getMin() {
+        int min = INT_MAX;
+        for ( int i =0;i<st.size();i++){
+            if(st[i]<min){
+                min = st[i];
+            }
+        }
+        return min;
+    }
+};
